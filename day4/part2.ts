f9387718ab3295db6d9cb73bc1a7ab6cb7e0d35c
Enes Kaplan <eneskaplan94@hotmail.com>
2022-12-04 13:44:31 +0300

    Day 4 - Part 2 completed

---
+++ day4/part2.ts
@@ -0,0 +1,23 @@
+export const x = "";
+
+const fs = require("fs");
+fs.readFile("./input.txt", "utf-8", (err, data) => {
+  if (err) return;
+
+  const pairs: string[] = data.split("\r\n");
+  let noOverlap = 0;
+  pairs.forEach((p) => {
+    const pair = p.split(",");
+    const pairOneRange = pair[0].split("-").map((m) => parseInt(m));
+    const pairTwoRange = pair[1].split("-").map((m) => parseInt(m));
+
+    if (
+			(pairOneRange[0] < pairTwoRange[0] && pairOneRange[1] < pairTwoRange[0]) ||
+			(pairOneRange[0] > pairTwoRange[1] && pairOneRange[1] > pairTwoRange[1])
+		) {
+      noOverlap++;
+    } 
+	});
+
+  console.log(pairs.length - noOverlap);
+});
